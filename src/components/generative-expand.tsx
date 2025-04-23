3fd50201ca832aa037eacf242ae2043743cde179
luyen <luyenluu003@gmail.com>
2025-04-23 15:39:35 +0700

    update

--- src/components/generative-expand.tsx
+++ src/components/generative-expand.tsx
@@ -1,7 +1,44 @@
-import type { FC } from 'react';
-
+import type { FC } from "react";
+import BackImage from "@/assets/images/generative-expand-back.webp";
+import FrontImage from "@/assets/images/generative-expand-front.webp";
 const GenerativeExpand: FC = () => {
-    return <></>;
+  return (
+    <section className="flex flex-col items-center justify-center gap-3 mt-16">
+      <div className="flex w-[90%] md:flex-row flex-col gap-10">
+        <div className="md:w-[40%]">
+          <p className="text-[12px] font-semibold tracking-[4px] text-[#7286ff] uppercase">
+            Generative AI
+          </p>
+          <p className="text-[28px] font-extrabold text-[#ffffffee]">
+            See more with AI Generative Expand!
+          </p>
+          <p className="text-sm leading-[20px] font-normal text-[#ffffffcc]">
+            Explore beyond the borders of your canvas with Generative Expand,
+            make your image fit in any aspect without cropping the best parts.
+            Just expand in any direction and the new content will blend
+            seamlessly with the image.
+          </p>
+          <button className="text-glow-2 mt-5 flex h-[44px] cursor-pointer items-center justify-center gap-2 rounded-full bg-[#5463bf] px-10 text-[14px] font-normal">
+            Test Generative Expand
+          </button>
+        </div>
+        <div className="md:w-[60%] pt-0 p-10">
+          <div className="group relative flex min-h-[400px] items-center justify-center">
+            <div className="relative flex justify-center items-center min-h-[600px] w-full">
+              <img
+                src={BackImage}
+                className="absolute z-2 m-auto w-full scale-70 rounded-3xl duration-300 ease-in-out group-hover:scale-100"
+              />
+              <img
+                src={FrontImage}
+                className="absolute z-4 md:w-[41.6666666%] h-auto rounded-3xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GenerativeExpand;
--- src/components/generative-fill.tsx
+++ src/components/generative-fill.tsx
@@ -1,7 +1,41 @@
-import type { FC } from 'react';
+import type { FC } from "react";
+import AfterImage from "@/assets/images/ai-generative-fill-after.webp";
+import BeforeImage from "@/assets/images/ai-generative-fill-before.webp";
+
 
 const GenerativeFill: FC = () => {
-    return <></>;
+    return (
+        <section className="mt-12 flex flex-col-reverse items-center justify-center gap-3 ">
+            <div className="flex w-[90%] md:flex-row flex-col-reverse gap-10">
+                <div className="md:w-[60%] p-10 pt-0">
+                    <div className="group relative flex min-h-[400px] items-center justify-center">
+                        <div className="relative min-h-[400px] min-w-[400px] flex items-center justify-center">
+                            <img className="absolute rounded-2xl tranform translate-x-[-25%] translate-y-[-5%] group-hover:translate-y-[-10%] group-hover:translate-x-[-35%] max-w-[250px] md:max-w-[330px] h-auto duration-300 ease-in-out"
+                                    src={BeforeImage}
+                                />
+                            <img className=" absolute rounded-2xl tranform translate-x-[25%] translate-y-[5%] max-w-[250px] md:max-w-[330px] group-hover:translate-y-[10%] group-hover:translate-x-[35%]  h-auto duration-300 ease-in-out"
+                                src={AfterImage}
+                            />
+                            </div>
+                    </div>
+                </div>
+                <div className="md:w-[40%]">
+                    <p className="text-[12px] font-semibold tracking-[4px] text-[#7286ff] uppercase">
+                        Generative AI
+                    </p>
+                    <p className="text-[28px] font-extrabold text-[#ffffffee]">
+                    Amazing AI Generative Fill!
+                    </p>
+                    <p className="text-sm leading-[20px] font-normal text-[#ffffffcc]">
+                    From concepting creative ideas to making complex edits and refinements, Generative Fill can help you quickly realize your vision while giving you complete control over every creation.
+                    </p>
+                    <button className="text-glow-2 mt-5 flex h-[44px] cursor-pointer items-center justify-center gap-2 rounded-full bg-[#5463bf] px-10 text-[14px] font-normal">
+                        Test Generative Fill
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default GenerativeFill;
