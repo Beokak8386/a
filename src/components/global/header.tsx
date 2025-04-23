d46e993e8669ca7c8bf0ca06439e777c47927e75
ovftank <ovftank@gmail.com>
2025-04-23 15:49:11 +0700

    chore: format code

--- src/components/global/header.tsx
+++ src/components/global/header.tsx
@@ -1,25 +1,57 @@
-import type { FC } from 'react';
-import logoImage from '@/assets/images/pixlr-logo.svg'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBell, faBars, faCrown, faUser } from '@fortawesome/free-solid-svg-icons';
+import type { FC } from "react";
+import logoImage from "@/assets/images/pixlr-logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faBell,
+    faBars,
+    faCrown,
+    faUser,
+} from "@fortawesome/free-solid-svg-icons";
 const Header: FC = () => {
-    return <div className='flex py-5 justify-center'>
-        <div className='max-w-[90%] w-full flex justify-between items-center'>
-        <div>
-            <img src={logoImage} />
-        </div>
-        <div className='flex gap-3 font-bold text-white text-sm justify-center items-center'>
-            <p className='py-2 px-4 hidden rounded-full hover:bg-[#ffffff22] cursor-pointer md:flex'>Pricing</p>
-            <p className='hidden md:flex justify-center items-center gap-1 py-2 px-4 rounded-full hover:bg-[#ffffff22] cursor-pointer'>  <FontAwesomeIcon icon={faUser} size='1x' className='w-5'/>Sign up / Log in</p>
-            <p className='hidden md:flex justify-center items-center gap-1 py-2 px-4 rounded-full hover:bg-[#ffffff22] cursor-pointer'>   <FontAwesomeIcon icon={faCrown} size='xl' className='w-5  text-yellow-500'/>Try premiums</p>
-            <div className='hidden md:flex'>
-            <FontAwesomeIcon icon={faBell} size='xl' className=' w-5 py-3 px-3 rounded-full hover:bg-[#ffffff22] cursor-pointer'/>
-
+    return (
+        <div className="flex justify-center py-5">
+            <div className="flex w-full max-w-[90%] items-center justify-between">
+                <div>
+                    <img  alt="" src={logoImage} />
+                </div>
+                <div className="flex items-center justify-center gap-3 text-sm font-bold text-white">
+                    <p className="hidden cursor-pointer rounded-full px-4 py-2 hover:bg-[#ffffff22] md:flex">
+                        Pricing
+                    </p>
+                    <p className="hidden cursor-pointer items-center justify-center gap-1 rounded-full px-4 py-2 hover:bg-[#ffffff22] md:flex">
+                        {" "}
+                        <FontAwesomeIcon
+                            icon={faUser}
+                            size="1x"
+                            className="w-5"
+                        />
+                        Sign up / Log in
+                    </p>
+                    <p className="hidden cursor-pointer items-center justify-center gap-1 rounded-full px-4 py-2 hover:bg-[#ffffff22] md:flex">
+                        {" "}
+                        <FontAwesomeIcon
+                            icon={faCrown}
+                            size="xl"
+                            className="w-5 text-yellow-500"
+                        />
+                        Try premiums
+                    </p>
+                    <div className="hidden md:flex">
+                        <FontAwesomeIcon
+                            icon={faBell}
+                            size="xl"
+                            className="w-5 cursor-pointer rounded-full px-3 py-3 hover:bg-[#ffffff22]"
+                        />
+                    </div>
+                    <FontAwesomeIcon
+                        icon={faBars}
+                        size="xl"
+                        className="w-5 cursor-pointer rounded-full px-3 py-3 hover:bg-[#ffffff22]"
+                    />
+                </div>
             </div>
-            <FontAwesomeIcon icon={faBars} size='xl' className='w-5 py-3 px-3 rounded-full hover:bg-[#ffffff22] cursor-pointer'/>
         </div>
-        </div>
-    </div>;
+    );
 };
 
 export default Header;
--- src/components/photo-editor.tsx
+++ src/components/photo-editor.tsx
@@ -1,30 +1,35 @@
-import UploadImage from '@/assets/images/image-editor.svg';
-import MagicImage from '@/assets/images/image-generator.svg';
+import UploadImage from "@/assets/images/image-editor.svg";
+import MagicImage from "@/assets/images/image-generator.svg";
 
-import type { FC } from 'react';
+import type { FC } from "react";
 
 const PhotoEditor: FC = () => {
-    return <>
-        <section className='flex flex-col  gap-3 justify-center items-center py-20 '>
-            <p className='bg-clip-text text-transparent text-6xl md:text-[84px] text-center font-black bg-linear-to-r/shorter from-[#8B3AD8] from-10% to-blue-400 uppercase'>Photo Editor</p>
-            <div className='pb-6'>
-                <p className='text-center font-semibold text-xl md:text-2xl text-gray-300'>AI Image Generator and AI Video Generator</p>
-            </div>
-            <div className='flex items-center justify-center gap-[30px] flex-col md:flex-row '>
-                <button className='bg-transparent cursor-pointer h-14 border-2 px-10 text-glow rounded-full gap-2 border-[rgba(150,92,201,0.342)] flex justify-center items-center'>
-                    <img src={UploadImage} className='w-[24px] h-[24px]' /> Open AI Photo Editor
-                </button>
-                <button className='bg-transparent cursor-pointer h-14 border-2 px-10 text-glow rounded-full gap-2 border-[rgba(150,92,201,0.342)] flex justify-center items-center'>
-                    <img src={MagicImage} className='w-[24px] h-[24px]' />  AI Image Generator
-                </button>
-            </div>
+    return (
+            <section className="flex flex-col items-center justify-center gap-3 py-20">
+                <p className="bg-linear-to-r/shorter from-[#8B3AD8] from-10% to-blue-400 bg-clip-text text-center text-6xl font-black text-transparent uppercase md:text-[84px]">
+                    Photo Editor
+                </p>
+                <div className="pb-6">
+                    <p className="text-center text-xl font-semibold text-gray-300 md:text-2xl">
+                        AI Image Generator and AI Video Generator
+                    </p>
+                </div>
+                <div className="flex flex-col items-center justify-center gap-[30px] md:flex-row">
+                    <button className="text-glow flex h-14 cursor-pointer items-center justify-center gap-2 rounded-full border-2 border-[rgba(150,92,201,0.342)] bg-transparent px-10">
+                        <img src={UploadImage} className="h-[24px] w-[24px]"  alt=""/>{" "}
+                        Open AI Photo Editor
+                    </button>
+                    <button className="text-glow flex h-14 cursor-pointer items-center justify-center gap-2 rounded-full border-2 border-[rgba(150,92,201,0.342)] bg-transparent px-10">
+                        <img src={MagicImage} className="h-[24px] w-[24px]"  alt=""/>{" "}
+                        AI Image Generator
+                    </button>
+                </div>
 
-            <button className='bg-[#8C54BD] cursor-pointer h-12 border-2 mt-5 px-10 text-glow-2 rounded-full gap-2 border-[rgba(150,92,201,0.342)] flex justify-center items-center'>
-                START 7 DAY TRIAL
-            </button>
-        </section>
-
-    </>;
+                <button className="text-glow-2 mt-5 flex h-12 cursor-pointer items-center justify-center gap-2 rounded-full border-2 border-[rgba(150,92,201,0.342)] bg-[#8C54BD] px-10">
+                    START 7 DAY TRIAL
+                </button>
+            </section>
+    );
 };
 
 export default PhotoEditor;
