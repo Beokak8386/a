3c7af2af204f18901c71ba19179c61ba804c3feb
ovftank <ovftank@gmail.com>
2025-04-23 10:51:02 +0700

    chore: init repo

---
+++ src/components/generative-ai-tools.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+const GenerativeAiTools: FC = () => {
+    return <>section 2</>;
+};
+
+export default GenerativeAiTools;
---
+++ src/components/generative-fill.tsx
@@ -0,0 +1,7 @@
+import type { FC } from 'react';
+
+const GenerativeFill: FC = () => {
+    return <></>;
+};
+
+export default GenerativeFill;
